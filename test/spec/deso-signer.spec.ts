4856a234247e4f592871ef2c9f4cb5bf2222e835
Jim Toth <jim@artby.city>
2022-11-08 15:47:57 -0500

    Adds spec test setup and related deps

---
+++ test/spec/deso-signer.spec.ts
@@ -0,0 +1,11 @@
+import 'mocha'
+import { expect } from 'chai'
+// import { createData } from 'arbundles'
+
+// import { DeSoSigner } from '../../src'
+
+describe('DeSo Signer', () => {
+  it('signs and verifies data items', () => {
+    expect.fail('Test Not Implemented')
+  })
+})
